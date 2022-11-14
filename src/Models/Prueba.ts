65cef0d5c64beaf35fbfd3105b8082eb9a8a382f
LuisMendezMoreno <jor.mendez.moreno@gmail.com>
2022-11-13 19:13:17 -0500

    ProjectInit

---
+++ src/Models/Prueba.ts
@@ -0,0 +1,28 @@
+
+// export interface User
+
+// {
+//   name: String,
+//   email: String,
+//   password: String,
+//   role: String,
+// }
+
+// const listUser:  = 
+// [
+//   {
+//     name: 'angelica',
+//     email: 'prubea',
+//     password: 'sasasasa',
+//     role: 'ssss',
+//   }
+  
+// ]
+
+
+// const data = listUser[0].
+
+
+
+
+
---
+++ src/Pages/NotFound/NotFounf.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFounf() {
+  return (
+    <div>
+        NotFound
+    </div>
+  )
+}

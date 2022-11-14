0708cb96eab2d4e3f0ea519e7b3e7f92144ac179
jorgeffmeza <jorgeffmeza@gmail.com>
2022-11-13 22:05:42 -0500

    styledComponents

---
+++ src/Components/Elements/Button/ButtonStyled.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+export default function ButtonStyled() {
+  return (
+    <Button variant="primary">Primary</Button>
+  )
+}
--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import ButtonStyled from '../../Components/Elements/Button/ButtonStyled'
 
 export default function Main() {
   return (
     <div>
       Main Layout
+      <ButtonStyled/>
       <Outlet />
     </div>
   )

646ffd52912ea5dfc01416d33f79e047b21e3369
isavarch <isabelevargas265@gmail.com>
2022-11-22 21:08:37 -0500

    update 6

---
+++ src/Pages/Home/Sections/Beneficios/Styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const BenefitStyled = styled.div `
+    background-image: url("Rectangle 972.png") ;
+    display: flex;
+    justify-content: end;
+    padding-right: 3.3rem;
+    color: #ffff;
+    font-family: 'Inter';
+    font-style: normal;
+    padding-left: 5rem;
+    position: relative;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+
+
+
+.text {
+    padding-right: 3.5rem;
+}    
+
+
+h1 {
+    padding-top: 4.4rem;
+    font-weight: 500;
+    padding-bottom: 3rem;
+}    
+
+p {
+    font-weight: 500;
+    font-size: 1.3rem;
+    display: flex;
+    padding-bottom: 1.5rem;
+}
+
+
+
+
+
+`;
+
+

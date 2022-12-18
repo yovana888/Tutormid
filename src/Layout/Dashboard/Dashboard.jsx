33cf351b80b9ac7c366077d62e9e5401af56f7b7
yovanav.delfosti <yovana.velasquez@delfosti.com>
2022-12-17 21:33:09 -0500

    fix routes

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Outlet } from 'react-router-dom';
-import NavbarDashboard from '../../Components/Groups/NavbarDashboard/NavbarDashboard'
+import NavbarDashboard from '../../Components/Groups/NavbarDashboard/NavbarDashboard';
+import { AuthContext } from '../../context/auth.context';
+import { Navigate } from "react-router-dom";
+
 export default function Dashboard() {
+  const { isLogin } = useContext(AuthContext);
+  if (!isLogin) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
         <NavbarDashboard/>

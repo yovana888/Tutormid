81bdbaa73400a95cb53f9b65dbedf383a5e59f9d
yovanav.delfosti <yovana.velasquez@delfosti.com>
2022-12-19 17:48:14 -0500

    fix banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import AdsTeachers from '../Home/Sections/AdsTeachers/AdsTeachers'
-import Banner from './Sections/Banner/Banner'
 import Beneficios from './Sections/Beneficios/Beneficios'
 import Steps from './Sections/Steps/Steps'
 import Reviews from './Sections/Reviews/Reviews'
+import { Container, ColumnText, ContainerImage } from './Styles'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import NewInputGroup from '../../Components/Groups/InputGroup/InputGroup';
+import Text from '../../Components/Elements/Text/Text';
 
 export default function Home() {  
   return (
     <>
-        <Banner/>
+        <Container>
+           <Row>
+                <Col>
+                    <ColumnText> 
+                        <Text name='Encuentra tu' name2='Profesor Particular'/>
+                        <Text name='En línea o presencial, comienza y agenda tus 
+                          lecciones de manera grupal o individual en el  
+                          horario que más se adpate a tí' typeText='subtitle'/>
+                        <NewInputGroup/>
+                    </ColumnText>
+                </Col>
+                <ContainerImage><img src="src/assets/img-banner.svg" alt="img-banner" /></ContainerImage>
+           </Row>
+        </Container>
         <Steps/>
         <AdsTeachers/>
         <Beneficios/>
--- src/Pages/Home/Styles.js
+++ src/Pages/Home/Styles.js
@@ -1,1 +1,45 @@
 
+import styled from "styled-components";
+import { colors } from "../../Styles/colors";
+import Col from "react-bootstrap/Col";
+
+export const Container = styled.div`
+  background: ${colors.bgLight};
+  padding: 30px 3.3rem;
+`;
+
+export const ContainerImage = styled(Col)`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  img {
+    width: 94%;
+  }
+  @media screen and (max-width: 750px) {
+    display: none;
+  }
+`;
+
+export const ColumnText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-top: 10%;
+  padding-bottom: 30px;
+  @media screen and (max-width: 750px) {
+    padding-top: 5%;
+    justify-content: center;
+    align-items: center;
+    text-align:center;
+  }
+`;
+
+export const Title = styled.h1`
+  color: ${colors.darkColor};
+`;
+
+export const SubTitle = styled.span`
+  color: ${colors.darkColor};
+  margin-bottom: 18px;
+  font-size:17px;
+`;
